4fffdd07f93167600362361226e84dbdedc622af
David Maull <d@network-sanity.com>
2020-11-09 11:07:09 -0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+/*
+
+
+
+*/
+
+
+use warp::Filter;
+
+#[tokio::main]
+
+
+async fn main() {
+
+    let register = warp::path("register")
+        .map(|| "Hello from register");
+
+    let login = warp::path("login")
+        .map(|| "Hello from login");
+
+    let logout = warp::path("logout")
+        .map(|| "Hello from logout");
+
+    let routes = register
+        .or(login)
+        .or(logout);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
